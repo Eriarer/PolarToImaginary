ec49523ed1642d01b704265e8547b8e7616bd42f
Eriarer <eriarer_melgoza+git@hotmail.com>
2023-11-23 20:04:35 -0600

    fixed Potencia

--- view/js/main.js
+++ view/js/main.js
@@ -6,15 +6,11 @@ var
   gridX, gridY,
   vectorX, vectorY = null;
 
-var ticks,
+var ticksX, ticksY,
   xDom, yDom,
   real, imaginario,
   side = 0;
-var vectors = [
-  [{ x: 0, y: 0 }, { x: 0, y: 0 }, color = "red"],
-  [{ x: 0, y: 0 }, { x: 0, y: 0 }, color = "blue"],
-  [{ x: 0, y: 0 }, { x: 0, y: 0 }, color = "green"]
-];
+var vectors = [];
 var vectorList = [];
 // definir el margin en terminos de em
 margin = {
@@ -54,7 +50,6 @@ function initGraph() {
   svg = d3.select("svg").append("svg")
     .attr("width", 0)
     .attr("height", 0);
-
 
   updateDom();
 
@@ -133,6 +128,12 @@ function updateGraph() {
 
   setScreensize();
 
+  updateDom();
+
+  // Actuaizar los vectores
+  for (var i = 0; i < vectors.length; i++) {
+    updateVector(i);
+  }
 
   svg.attr("width", width)
     .attr("height", height);
@@ -165,25 +166,31 @@ function updateGraph() {
 }
 
 function updateSclae() {
-  switch (true) {
-    case (width >= 0 && width < 768):
-      ticks = 10;
-      break;
-    default:
-      ticks = 20;
-  }// switch
-  var tickStepX = (2 * xDom) / ticks;
-  var tickStepY = (2 * yDom) / ticks;
+  // cambiar los ticks segun el dominio
+  if (xDom <= 5) {
+    ticksX = xDom * 2;
+  } else {
+    ticksX = 10;
+  }
+
+  if (yDom <= 5) {
+    ticksY = yDom * 2;
+  } else {
+    ticksY = 10;
+  }
+
+  var tickStepX = (2 * xDom) / ticksX;
+  var tickStepY = (2 * yDom) / ticksY;
   //forzar la escala a utilizar los ticks
   xScale = d3.axisBottom(xAxis)
-    .ticks(ticks)
+    .ticks(ticksX)
     .tickValues(d3.range(-xDom, xDom + tickStepX, tickStepX))
     .tickFormat(function (d) {
       return d === 0 ? "" : d;
     });
   // Dentro de la función modifyScale()
   yScale = d3.axisLeft(yAxis)
-    .ticks(ticks)
+    .ticks(ticksY)
     .tickValues(d3.range(-yDom, yDom + tickStepY, tickStepY))
     .tickFormat(function (d) {
       if (d === 0) {
@@ -195,29 +202,34 @@ function updateSclae() {
 }// modifyScale
 
 function drawGrid() {
-  if (ticks <= gridX.length) {
+  drawGridX();
+  drawGridY();
+}
+
+function drawGridX() {
+  if (ticksX <= gridX.length) {
     for (var i = 0; i < gridX.length; i++) {
       gridX[i]
         .transition()
         .duration(1000)
-        .attr("y1", i * ((height - margin.left - margin.right) / ticks))
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksX))
         .attr("x2", width - margin.right - margin.left)
-        .attr("y2", i * (height - margin.left - margin.right) / ticks)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksX)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
       gridY[i]
         .transition()
         .duration(1000)
-        .attr("x1", i * ((width - margin.top - margin.bottom) / ticks))
-        .attr("x2", i * ((width - margin.top - margin.bottom) / ticks))
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksX))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksX))
         .attr("y2", height - margin.top - margin.bottom)
 
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     }
     // Eliminar elementos extra
-    gridX.splice(ticks + 1, gridX.length - ticks).forEach(function (el) {
+    gridX.splice(ticksX + 1, gridX.length - ticksX).forEach(function (el) {
       el.remove();
     });
-    gridY.splice(ticks + 1, gridY.length - ticks).forEach(function (el) {
+    gridY.splice(ticksX + 1, gridY.length - ticksX).forEach(function (el) {
       el.remove();
     });
   } else {
@@ -225,19 +237,19 @@ function drawGrid() {
       gridX[i]
         .transition()
         .duration(1000)
-        .attr("y1", i * ((height - margin.left - margin.right) / ticks))
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksX))
         .attr("x2", width - margin.right - margin.left)
-        .attr("y2", i * (height - margin.left - margin.right) / ticks)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksX)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
       gridY[i]
         .transition()
         .duration(1000)
-        .attr("x1", i * ((width - margin.top - margin.bottom) / ticks))
-        .attr("x2", i * ((width - margin.top - margin.bottom) / ticks))
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksX))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksX))
         .attr("y2", height - margin.top - margin.bottom)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     }
-    for (var i = gridX.length; i < ticks + 1; i++) {
+    for (var i = gridX.length; i < ticksX + 1; i++) {
       gridX.push(
         svg.append("line")
           .attr("class", "grid-line")
@@ -266,90 +278,162 @@ function drawGrid() {
       gridX[i]
         .transition()
         .duration(1000)
-        .attr("y1", i * ((height - margin.left - margin.right) / ticks))
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksX))
         .attr("x2", width - margin.right - margin.left)
-        .attr("y2", i * (height - margin.left - margin.right) / ticks)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksX)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
       gridY[i]
         .transition()
         .duration(1000)
-        .attr("x1", i * ((width - margin.top - margin.bottom) / ticks))
-        .attr("x2", i * ((width - margin.top - margin.bottom) / ticks))
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksX))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksX))
         .attr("y2", height - margin.top - margin.bottom)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     }
   }
 }
 
-function initVector(origin, end, color = "black") {
-  return svg.append("line")
-    .attr("class", "vector")
-    .attr("x1", origin.x)
-    .attr("y1", -origin.y)
-    .attr("x2", end.x)
-    .attr("y2", -end.y)
-    .attr("stroke", color)
-    .attr("stroke-width", 4)
-    .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
+function drawGridY() {
+  if (ticksY <= gridX.length) {
+    for (var i = 0; i < gridX.length; i++) {
+      gridX[i]
+        .transition()
+        .duration(1000)
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksY))
+        .attr("x2", width - margin.right - margin.left)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksY)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      gridY[i]
+        .transition()
+        .duration(1000)
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("y2", height - margin.top - margin.bottom)
+
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+    // Eliminar elementos extra
+    gridX.splice(ticksY + 1, gridX.length - ticksY).forEach(function (el) {
+      el.remove();
+    });
+    gridY.splice(ticksY + 1, gridY.length - ticksY).forEach(function (el) {
+      el.remove();
+    });
+  } else {
+    for (var i = 0; i < gridX.length; i++) {
+      gridX[i]
+        .transition()
+        .duration(1000)
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksY))
+        .attr("x2", width - margin.right - margin.left)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksY)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      gridY[i]
+        .transition()
+        .duration(1000)
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("y2", height - margin.top - margin.bottom)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+    for (var i = gridX.length; i < ticksY + 1; i++) {
+      gridX.push(
+        svg.append("line")
+          .attr("class", "grid-line")
+          .attr("x1", 0)
+          .attr("y1", 0)
+          .attr("x2", 0)
+          .attr("y2", 0)
+          .attr("stroke", "gray")
+          .attr("stroke-width", 1)
+          .attr("stroke-dasharray", "1, 1")
+          .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+      );
+      gridY.push(
+        svg.append("line")
+          .attr("class", "grid-line")
+          .attr("x1", 0)
+          .attr("y1", 0)
+          .attr("x2", 0)
+          .attr("y2", 0)
+          .attr("stroke", "gray")
+          .attr("stroke-width", 1)
+          .attr("stroke-dasharray", "1, 1")
+          .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+      );
+
+      gridX[i]
+        .transition()
+        .duration(1000)
+        .attr("y1", i * ((height - margin.left - margin.right) / ticksY))
+        .attr("x2", width - margin.right - margin.left)
+        .attr("y2", i * (height - margin.left - margin.right) / ticksY)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      gridY[i]
+        .transition()
+        .duration(1000)
+        .attr("x1", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("x2", i * ((width - margin.top - margin.bottom) / ticksY))
+        .attr("y2", height - margin.top - margin.bottom)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+  }
 }
 
-function updateVector(vector, origin, end) {
-  if (vector === null || vector === undefined || origin === null || origin === undefined || end === null || end === undefined) {
+function initVector(origin = { x: 0, y: 0 }, end = { x: 0, y: 0 }, color = "black") {
+  // añaadir el vector al arreglo de vectores
+  vectors.push([origin, end, color]);
+  // añadir el vector a la lista de vectores
+  vectorList.push(
+    svg.append("line")
+      .attr("class", "vector")
+      .attr("x1", origin.x)
+      .attr("y1", -origin.y)
+      .attr("x2", end.x)
+      .attr("y2", -end.y)
+      .attr("stroke", color)
+      .attr("stroke-width", 4)
+      .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+  );
+}
+
+function changeVector(pos, origin = { x: 0, y: 0 }, end = { x: 0, y: 0 }, color = null) {
+  // verificar que el vector exista en vectors
+  if (pos < 0 || pos >= vectors.length) {
     return;
   }
+  // modificar el vector en vectors
+  vectors[pos][0] = origin;
+  vectors[pos][1] = end;
+  if (color !== null) {
+    vectors[pos][2] = color;
+  }
+  updateGraph();
+}
+
+function updateVector(pos) {
+  // verificar que el vector exista en vectors
+  if (pos < 0 || pos >= vectors.length) {
+    return;
+  }
+  // conseguir los parametros del vector
+  var origin = vectors[pos][0];
+  var end = vectors[pos][1];
+  var color = vectors[pos][2];
+
   // Ajustar las coordenadas en función del dominio
   var xScaleFactor = xDom === 0 ? 0 : (width - margin.left - margin.right) / (2 * xDom);
   var yScaleFactor = yDom === 0 ? 0 : (height - margin.top - margin.bottom) / (2 * yDom);
 
-  vector
+  vectorList[pos]
     .transition()
     .duration(1000)
     .attr("x1", origin.x * xScaleFactor)
     .attr("y1", -origin.y * yScaleFactor)
     .attr("x2", end.x * xScaleFactor)
     .attr("y2", -end.y * yScaleFactor)
+    .attr("stroke", color)
     .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
-}
-
-// recorrer los elementos del arreglo y ajustar el domino de la gráfica de los ejes
-// al valor mayor de los elementos del arreglo
-function updateDom() {
-  var xMax = 0;
-  var yMax = 0;
-  for (var i = 0; i < vectors.length; i++) {
-    if (Math.abs(vectors[i][0].x) > xMax) {
-      xMax = Math.abs(vectors[i][0].x);
-    }
-    if (Math.abs(vectors[i][1].x) > xMax) {
-      xMax = Math.abs(vectors[i][1].x);
-    }
-    if (Math.abs(vectors[i][0].y) > yMax) {
-      yMax = Math.abs(vectors[i][0].y);
-    }
-    if (Math.abs(vectors[i][1].y) > yMax) {
-      yMax = Math.abs(vectors[i][1].y);
-    }
-  }
-  for (var i = 1; true; i *= 10) {
-    // si el valor se sale del maximo del dominio actual
-    // entonces se aumenta el dominio
-    if (i * 10 < xMax) {
-      continue;
-    }
-    // encontrar el numero mínimo multiplo del dominio que sea mayor al valor
-    xDom = xMax % i === 0 ? xMax : xMax + (i - xMax % i);
-    break;
-  }
-  for (var i = 1; true; i *= 10) {
-    // si el valor se sale del maximo del dominio actual
-    // entonces se aumenta el dominio
-    if (i * 10 < yMax) {
-      continue;
-    }
-    // encontrar el numero mínimo multiplo del dominio que sea mayor al valor
-    yDom = yMax % i === 0 ? yMax : yMax + (i - yMax % i);
-    break;
-  }
 }
 
 function removeAllVectors() {
@@ -367,12 +451,76 @@ function removeAllVectors() {
       .attr("y2", 0);
   }
   sleep(1000).then(() => {
-    for (var i = 0; i < vectorList.length; i++) {
-      vectorList[i].remove();
-    }
-    vectorList = [];
+    isDelitingVectors = false;
   });
-  isDelitingVectors = false;
+}
+
+// recorrer los elementos del arreglo y ajustar el domino de la gráfica de los ejes
+// al valor mayor de los elementos del arreglo
+function updateDom() {
+  if (vectors.length === 0) {
+    xDom = 50;
+    yDom = 50;
+    return;
+  }
+  var xMax = 0;
+  var yMax = 0;
+  for (var i = 0; i < vectors.length; i++) {
+    if (Math.abs(vectors[i][0].x) > xMax) {
+      xMax = Math.abs(vectors[i][0].x);
+    }
+    if (Math.abs(vectors[i][1].x) > xMax) {
+      xMax = Math.abs(vectors[i][1].x);
+    }
+    if (Math.abs(vectors[i][0].y) > yMax) {
+      yMax = Math.abs(vectors[i][0].y);
+    }
+    if (Math.abs(vectors[i][1].y) > yMax) {
+      yMax = Math.abs(vectors[i][1].y);
+    }
+  }
+  if (xMax === 0) {
+    xMax = 50;
+  } else if (xMax <= 1) {
+    xMax = 1;
+  } else if (xMax <= 5) {
+    xMax = 5;
+  } else if (xMax <= 10) {
+    xMax = 10;
+  } else {
+    xMax = siguienteDivisible10(xMax);
+  }
+  if (yMax === 0) {
+    yMax = 50;
+  } else if (yMax <= 1) {
+    yMax = 1;
+  } else if (yMax <= 5) {
+    yMax = 5;
+  } else if (yMax <= 10) {
+    yMax = 10;
+  } else {
+    yMax = siguienteDivisible10(yMax);
+  }
+  max = Math.max(xMax, yMax);
+  xDom = max;
+  yDom = max;
+}
+
+// encontrar el siguiente numero divisible entre la potencia de 10 mas cercana
+// ejemplo 28 -> 30,  100 -> 100,  1568 -> 2000
+function siguienteDivisible10(n) {
+  for (var i = 1; true; i++) {
+    var potencia = Math.pow(10, i);
+    if (n > potencia * 10) {
+      continue;
+    }
+
+    for (var j = 1; j < 10; j++) {
+      if (n < potencia * j) {
+        return potencia * j;
+      }
+    }
+  }
 }
 
 function sleep(ms) {
